ae3b23f37ee2c225f0e27a4a8295f8d9678b6a83
Chris McGehee <cmcgehee@datto.com>
2022-01-12 21:28:08 -0800

    More integration tests

--- tests/integration/lyncser_client.py
+++ tests/integration/lyncser_client.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import tempfile
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 config_dir_container = '/lyncser_config'
 data_dir_container = '/lyncser_data'
@@ -17,17 +17,58 @@ class LyncserClient(object):
             self.container_id, 
             'lyncser'] + args + ['--log-level=debug'],
         check=True)
+
     
-    def write_data_file(self, filename: str, content: str):
+    def set_global_config(self, files_to_sync: Dict[str, List[str]]) -> None:
+        files_str = '\n    - '.join([ os.path.join(data_dir_container, file) for file in files_to_sync ])
+        global_config = "paths:\n"
+        for tag, files in files_to_sync.items():
+            files_str = '\n    - '.join([ os.path.join(data_dir_container, file) for file in files ])
+            global_config += f"  {tag}:\n    - {files_str}\n"
+        self.write_config_file('globalConfig.yaml', global_config)
+    
+    def set_local_tags(self, tags: List[str]) -> None:
+        self.write_config_file('localConfig.yaml', 'tags:\n  - ' + '\n  - '.join(tags) + '\n')
+    
+    def write_data_file(self, filename: str, content: str) -> None:
         self._write_file(f'{data_dir_container}/{filename}', content)
 
-    def write_config_file(self, filename: str, content: str):
+    def write_config_file(self, filename: str, content: str) -> None:
         self._write_file(f'{config_dir_container}/{filename}', content)
+    
+    def create_data_dir(self, dir_name: str) -> None:
+        subprocess.run([
+            'docker',
+            'exec',
+            self.container_id,
+            'mkdir',
+            f'{data_dir_container}/{dir_name}'],
+        check=True)
+    
+    def data_file_exists(self, filename: str) -> bool:
+        result = subprocess.run([
+            'docker',
+            'exec',
+            self.container_id,
+            'test',
+            '-f',
+            f'{data_dir_container}/{filename}'
+        ])
+        return result.returncode == 0
 
     def get_data_file_content(self, filename: str) -> str:
         return self._get_file_content(f'{data_dir_container}/{filename}')
+    
+    def delete_data_file(self, filename: str) -> None:
+        subprocess.run([
+            'docker',
+            'exec',
+            self.container_id,
+            'rm',
+            f'{data_dir_container}/{filename}'],
+        check=True)
 
-    def _write_file(self, filename: str, content: str):
+    def _write_file(self, filename: str, content: str) -> None:
         with tempfile.NamedTemporaryFile(mode='w+') as f:
             f.write(content)
             f.flush()
@@ -49,26 +90,18 @@ class LyncserClient(object):
         return result.stdout.decode('utf-8')
 
 
-def set_global_config(files_to_sync: List[str], client: LyncserClient):
-    files_str = '\n    - '.join([ os.path.join(data_dir_container, file) for file in files_to_sync ])
-    global_config = f"""paths:
-  all:
-    - {files_str}
-"""
-    client.write_config_file('globalConfig.yaml', global_config)
-
-def create_and_prep_clients(files_to_sync: List[str]) -> Tuple[LyncserClient, LyncserClient]:
-    client1 = create_client()
-    client2 = create_client()
+def create_and_prep_clients(files_to_sync: Dict[str, List[str]]) -> Tuple[LyncserClient, LyncserClient, LyncserClient]:
     encryption_key = '166d8e96ae29d01dd155f840ac61657acfaa63bc24d15457183e9da03d33ef56'
-    client1.write_config_file('encryption.key', encryption_key)
-    client2.write_config_file('encryption.key', encryption_key)
-    client1.run_lyncser(['deleteAllRemoteFiles', '-y'])
+    clients_to_create = 3
+    clients = ()
+    for _ in range(clients_to_create):
+        client = create_client()
+        client.write_config_file('encryption.key', encryption_key)
+        client.set_global_config(files_to_sync)
+        clients += (client,)
 
-    set_global_config(files_to_sync, client1)
-    set_global_config(files_to_sync, client2)
-
-    return client1, client2
+    clients[0].run_lyncser(['deleteAllRemoteFiles', '-y'])
+    return clients
 
 def create_client() -> LyncserClient:
     result = subprocess.run([
--- tests/integration/test_lyncser.py
+++ tests/integration/test_lyncser.py
@@ -1,42 +1,98 @@
 import random
 
-from .lyncser_client import create_and_prep_clients, set_global_config
+from .lyncser_client import create_and_prep_clients, LyncserClient
 
 
 def test_lot_of_stuff():
     file1 = f'test{random.randrange(0, 100000)}.txt'
-    client1, client2 = create_and_prep_clients([file1])
+    files_to_sync = { 'all': [file1]}
+    client1, client2, client3 = create_and_prep_clients(files_to_sync)
 
-    # Upload new file and download it on the other client
+    # Upload new file and download it on client2
     file1_contents = 'test1'
     client1.write_data_file(file1, file1_contents)
     client1.run_lyncser(['sync'])
     client2.run_lyncser(['sync'])
     assert client2.get_data_file_content(file1) == file1_contents
 
-    # Modify the file on the first client and download it on the other client
-    file1_contents = 'test2'
+    # Modify the file on client1 and download it on client2
+    file1_contents = 'test1_modified'
     client1.write_data_file(file1, file1_contents)
     client1.run_lyncser(['sync'])
     client2.run_lyncser(['sync'])
     assert client2.get_data_file_content(file1) == file1_contents
 
-    # Modify the file on the second client and download it on the first client
-    file1_contents = 'test3'
+    # Modify the file on client2 and download it on client1
+    file1_contents = 't'
     client2.write_data_file(file1, file1_contents)
     client2.run_lyncser(['sync'])
     client1.run_lyncser(['sync'])
     assert client2.get_data_file_content(file1) == file1_contents
 
-    # Add a second file on the first client and make sure it gets downloaded on the other side
+    # Add file2 on client1 and make sure it gets downloaded on client2
     while True:
         file2 = f'test{random.randrange(0, 100000)}.txt'
         if file1 != file2:
             break
     file2_contents = 'test4'
     client1.write_data_file(file2, file2_contents)
-    set_global_config([file1, file2], client1)
+    files_to_sync = { 'all': [file1, file2]}
+    client1.set_global_config(files_to_sync)
     client1.run_lyncser(['sync'])
     client2.run_lyncser(['sync'])
-    client2.run_lyncser(['sync']) # Run twice because it needs to pick up the change in the global config
+    client2.run_lyncser(['sync']) # Run again to pick up the change in the global config
     assert client2.get_data_file_content(file2) == file2_contents
+
+    # Delete file2 on the client1 and make sure it doesn't gets deleted on client2
+    client1.delete_data_file(file2)
+    client1.run_lyncser(['sync'])
+    client2.run_lyncser(['sync'])
+    assert client2.get_data_file_content(file2) == file2_contents
+    assert not client1.data_file_exists(file2)
+
+    # Remove file2 from being synced on client2 and make sure it's no longer synced
+    files_to_sync = { 'all': [file1], 'client1and3': [file2]}
+    client1.set_global_config(files_to_sync)
+    client1.set_local_tags(['all', 'client1and3'])
+    client3.set_local_tags(['all', 'client1and3'])
+    # Propagate the global config to all clients
+    client1.run_lyncser(['sync'])
+    client2.run_lyncser(['sync'])
+    client3.run_lyncser(['sync'])
+    file2_contents_client2 = file2_contents
+    file2_contents_client1 = 'test5'
+    client1.write_data_file(file2, file2_contents_client1)
+    client1.run_lyncser(['sync'])
+    client2.run_lyncser(['sync'])
+    client3.run_lyncser(['sync'])
+    assert client2.get_data_file_content(file2) == file2_contents_client2
+    assert client3.get_data_file_content(file2) == file2_contents_client1
+    file2_contents_client2 = 'test6'
+    client2.write_data_file(file2, file2_contents_client2)
+    client2.run_lyncser(['sync'])
+    client1.run_lyncser(['sync'])
+    client3.run_lyncser(['sync'])
+    assert client1.get_data_file_content(file2) == file2_contents_client1
+    assert client3.get_data_file_content(file2) == file2_contents_client1
+
+    _test_directory_stuff(client1, client2)
+
+def _test_directory_stuff(client1: LyncserClient, client2: LyncserClient) -> None:
+    # Sync a directory
+    dir1 = 'test_dir'
+    client1.create_data_dir(dir1)
+    files_to_sync = { 'all': [dir1]}
+    client1.set_global_config(files_to_sync)
+    dir1_file1 = f'{dir1}/test1.txt'
+    dir1_file1_contents = 'test1'
+    client1.write_data_file(dir1_file1, dir1_file1_contents)
+    client1.run_lyncser(['sync'])
+    client2.run_lyncser(['sync'])
+    client2.run_lyncser(['sync']) # Run again to pick up the change in the global config
+    assert client2.get_data_file_content(dir1_file1) == dir1_file1_contents
+
+    # Delete file in directory on client2 and make sure it's not deleted on client1
+    client2.delete_data_file(dir1_file1)
+    client2.run_lyncser(['sync'])
+    client1.run_lyncser(['sync'])
+    assert client1.get_data_file_content(dir1_file1) == dir1_file1_contents
